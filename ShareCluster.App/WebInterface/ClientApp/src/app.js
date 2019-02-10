2a842ab6051f6c8e06a8a32d9a39e0406bf6bb23
Tomas Jecha <tomas@jecha.net>
2019-02-10 13:23:55 +0100

    React app base

---
+++ ShareCluster.App/WebInterface/ClientApp/src/app.js
@@ -0,0 +1,3 @@
+import 'bootstrap';
+import './styles/app.scss';
+import FormContainer from "./components/container/FormContainer.jsx";
---
+++ ShareCluster.App/WebInterface/ClientApp/webpack.config.js
@@ -0,0 +1,81 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var path = require("path");
+
+module.exports = {
+    output: {
+      path: path.resolve(__dirname, "./../wwwroot/"),
+      publicPath: "",
+      filename: "[name].bundle.js"
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, "./../wwwroot/"),
+      openPage: 'webpack-dev-server/index.html'
+    },
+    entry: './src/app.js',
+    module: {
+      rules: [
+
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: { minimize: true }
+            }
+          ]
+        },
+
+        {
+          test: /\.(scss)$/,
+          use: [
+            {
+              // // Adds CSS to the DOM by injecting a `<style>` tag
+              // loader: 'style-loader'
+
+              // separate files with styles
+              loader: MiniCssExtractPlugin.loader
+            },
+            {
+              // Interprets `@import` and `url()` like `import/require()` and will resolve them
+              loader: 'css-loader'
+            },
+            {
+              // Loader for webpack to process CSS with PostCSS
+              loader: 'postcss-loader',
+              options: {
+                plugins: function () {
+                  return [
+                    require('autoprefixer')
+                  ];
+                }
+              }
+            },
+            {
+              // Loads a SASS/SCSS file and compiles it to CSS
+              loader: 'sass-loader'
+            }
+          ]
+        }
+
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+      })
+    ]
+  };
