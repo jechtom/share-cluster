2d8dd69d5b9c7a219e021469ccc3d37c4eace1de
Jecha Tomas <jecha@avast.com>
2019-04-08 09:27:18 +0200

    Added missing change (VS Code crashed internally somehow)

--- ShareCluster.App/WebInterface/ClientApp/src/components/CreatePackage.jsx
+++ ShareCluster.App/WebInterface/ClientApp/src/components/CreatePackage.jsx
@@ -2,13 +2,30 @@ import React, { Component } from "react";
 import * as Commands from '../actions/commands'
 import { connect } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import BasicLink from './presentational/BasicLink.jsx'
+import { Link } from 'react-router-dom'
 
-const CreatePackage = ({ state, handleChange, handleChangeCheck, handleChangeType, submit }) => (
+function CreatePackage({ state, handleChange, handleChangeCheck, handleChangeType, submit, handleWithoutGroup }) {
+  const Header = () => {
+    if(state.group_use) {
+      return <div>
+        <h2><FontAwesomeIcon icon="folder-plus" /> New Version of { state.group_name }</h2>
+        <p><FontAwesomeIcon icon="info" /> You are creating package that is linked to a previous version. Also you can create <BasicLink onClick={handleWithoutGroup}>new package</BasicLink>.</p>
+      </div>
+    } else {
+      return <div>
+        <h2><FontAwesomeIcon icon="folder-plus" /> New Package</h2>
+        <p><FontAwesomeIcon icon="info" /> You are creating new package. To create new version of existing package use command on <Link to={"/packages"}>packages</Link> page.</p>
+      </div>
+    }
+  }
+
+  return (
     <div>
-      <h1><FontAwesomeIcon icon="folder-plus" /> Create Package</h1>
+      <Header />
       <form id="create-package-form" className="row container-fluid" onSubmit={ event => submit(event, state)}>
         <div className="row">
-          <div className="form-group col-lg-8 col-md-12">
+           <div className="form-group col-lg-8 col-md-12">
             <label for="path">Folder or file path</label>
             <input
               type="text"
@@ -53,7 +70,7 @@ const CreatePackage = ({ state, handleChange, handleChangeCheck, handleChangeTyp
             </div>
             <div className="radio">
               <label>
-                <input type="radio" name="package_type"value="reference" disabled checked={state.package_type == "reference"} onChange={handleChangeType} />Create reference to folder <i>(not yet supported)</i>
+                <input type="radio" name="package_type"value="reference" checked={state.package_type == "reference"} onChange={handleChangeType} />Create reference to folder
                 <small id="nameHelp" class="form-text text-muted">Only reference to given folder is created. This is recommended for large immutable files - like archives or disk images. Smaller files in folder can make upload slower and any changes to data will result in broken package (cannot be downloaded).</small>
               </label>
             </div>
@@ -61,8 +78,10 @@ const CreatePackage = ({ state, handleChange, handleChangeCheck, handleChangeTyp
         </div>
         <button type="submit" className="btn btn-primary">Create and publish package</button>
       </form>
-    </div>
-)
+    </div>)
+  }
+
+
 
 const mapStateToProps = state => ({
     state: state.CreatePackage
@@ -72,7 +91,8 @@ const mapDispatchToProps = dispatch => ({
   handleChange: (event) => dispatch(Commands.create_package_form_change(event.target.name, event.target.value)),
   handleChangeCheck: (event) => dispatch(Commands.create_package_form_change(event.target.name, event.target.checked)),
   handleChangeType: (event) => dispatch(Commands.create_package_form_change(event.target.name, event.target.value)),
-  submit: (event, state) => { event.preventDefault(); dispatch(Commands.create_package_form_submit(state)); }
+  handleWithoutGroup: (event) => dispatch(Commands.create_package_form_without_group()),
+  submit: (event, state) => { event.preventDefault(); dispatch(Commands.create_package_form_submit(state)); console.log("AD"); }
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreatePackage);
